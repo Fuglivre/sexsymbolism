14052d89bf4827aa3e1202f93a5e59bd4ef8eafe
Fuglivre <qiang.huang.16@ucl.ac.uk>
2017-04-19 21:35:18 +0100

    Add files via upload

---
+++ docs/js/index.js
@@ -0,0 +1,137 @@
+function encode_utf8(s) {
+  return unescape(encodeURIComponent(s));
+}
+
+function decode_utf8(s) {
+  return decodeURIComponent(escape(s));
+}
+
+$('#quiz').quiz({
+  //resultsScreen: '#results-screen',
+  //counter: false,
+  //homeButton: '#custom-home',
+  counterFormat: 'Question %current of %total',
+  questions: [
+    {
+      'q': '1. Christianity has used the symbol of ichthys <img src="images/ichthys.png" height="16px"></img> (the Greek word for fish) to convey theological messages. However,  it previously had another meaning. What do you think it might have been?',
+      'options': [
+        'A.	Greek symbol of a penis',
+        'B.	Paganist symbol of the vulva',
+		'C.	Ancient Roman symbol of sexual intercourse',
+		'D.	Celtic symbol of male power'
+      ],
+      'correctIndex': 1,
+      'correctResponse': '<b>Right answer!</b> Right answer! Before Christianity, Ancient Greeks, Ancient Romans, and Pagans used the fish symbol. For Pagans, it represented fertility or more specifically the female reproductive organs. In the early days of Christianity, the fish, attracted little suspicion, making it a perfect symbol to represent their faith whilst avoiding persecution.',
+      'incorrectResponse': '<b>Wrong!</b> The correct answer is <b>B</b>. Before Christianity, Ancient Greeks, Ancient Romans, and Pagans used the fish symbol. For Pagans, it represented fertility or more specifically the female reproductive organs. In the early days of Christianity, the fish, attracted little suspicion, making it a perfect symbol to represent their faith whilst avoiding persecution.'
+    },
+    {
+      'q': '2. Which of the followings is the most-tweeted emoji in France?',
+	  'picture':'http://emojipedia-us.s3.amazonaws.com/content/2016/09/23/new-whatsapp-emojis.jpg',
+      'options': [
+        'A.	🍆, The French are truly naughty about sex',
+        'B.	😂, The French are perhaps embarrassed about this kind of issue',
+        'C.	💘, Cupid has had a big effect on France',
+		'D.	👫, The French are known for their romance'
+      ],
+      'correctIndex': 2,
+      'correctResponse': '<b>Correct!</b> According to the newspaper, Metro News, The most tweeted emoji in the U.K. is 😩, while the Japanese and Italians\' most loved emoji is the heart ❤️.',
+      'incorrectResponse': '<b>Wrong answer!</b> The correct answer is <b>C</b>. According to the newspaper, Metro News, in 2016, The most tweeted emoji in the U.K. is 😩, while the Japanese and Italians\' most loved emoji is the heart ❤️.'
+    },
+    {
+      'q': '3. In Greek mythology, Zeus assumed all the following forms to seduce mortals, except ...',
+      'options': [
+        'A.	a snake 🐍',
+        'B.	an eagle 🦅',
+		'C.	a bull 🐂',
+		'D.	an ant 🐜'
+      ],
+      'correctIndex': 0,
+      'correctResponse': '<b>Correct!</b> Zeus took the form of a bull to abduct Europa. Eurymedousa, a princess of Phthiotis, was seduced by Zeus in the form of an ant. Zeus took away Ganymede, a Trojan boy, to be his lover and the cupbearer of Gods.',
+      'incorrectResponse': '<b>Wrong!</b> The correct answer is <b>A</b>. Zeus took the form of a bull to abduct Europa. Eurymedousa, a princess of Phthiotis, was seduced by Zeus in the form of an ant. Zeus took away Ganymede, a Trojan boy, to be his lover and the cupbearer of Gods.'
+    },
+    {
+      'q': '4. The neurotransmitters in some food are the same ones that are released naturally during sexual intercourse. This is true for ...',
+      'options': [
+        'A.	Oysters',
+        'B.	Strawberries',
+        'C.	Beef',
+		'D.	Chocolate'
+      ],
+      'correctIndex': 3,
+      'correctResponse': '<b>Good choice!</b> The stimulant phenylethylamine (PEA) is produced in greater amounts during sexual intercourse. PEA is also found in cocoa beans and improves energy and concentration levels and boosts mood. A deficiency in PEA can lead to low mood.',
+      'incorrectResponse': '<b>Perhaps not!</b> The correct answer is <b>D</b>. The stimulant phenylethylamine (PEA) is produced in greater amounts during sexual intercourse. PEA is also found in cocoa beans and improves energy and concentration levels and boosts mood. A deficiency in PEA can lead to low mood.'
+    },
+	{
+      'q': '5. The six colours on the LGBTQ community\'s rainbow flag each have their own meaning. Red, orange, yellow, green, indigo and violet, respectively mean:',
+      'options': [
+        'A.	Desire, Passion, Sunlight, Peace, Serenity and Individuality',
+        'B.	Passion, Healing, Life, Earth, Sky and Charm',
+        'C.	Life, Healing, Sunlight, Nature, Serenity and Spirit',
+		'D.	Love, Passion, Past, Present, Future and Dream'
+      ],
+      'correctIndex': 2,
+      'correctResponse': '<b>Correct!</b> The original gay pride flag flew in the San Francisco Gay Freedom Day Parade on June 25, 1978. Its creator, Gilbert Baker, may have been inspired by Judy Garland singing \'Over the Rainbow\'. The original design of the flag included eight colours, with a hot pink stripe on the top representing \'sex\'.',
+      'incorrectResponse': '<b>Wrong!</b> The correct answer is <b>C</b>. The original gay pride flag flew in the San Francisco Gay Freedom Day Parade on June 25, 1978. Its creator, Gilbert Baker, may have been inspired by Judy Garland singing \'Over the Rainbow\'. The original design of the flag included eight colours, with a hot pink stripe on the top representing \'sex\'.'
+    },
+    {
+      'q': '6. Men holding hands as friends is rare in many parts of the world. But in which of the following country, can two men hold hands without any romantic connotations?',
+      'options': [
+        'A.	China 🇨🇳',
+        'B.	Columbia 🇨🇴',
+        'C.	Poland 🇵🇱',
+		'D.	Lenanon 🇱🇧'
+      ],
+      'correctIndex': 3,
+      'correctResponse': '<b>Yes!</b> In this part of the world, affection between men is common, and without sexual connotation. In fact, if a man chooses not to touch another man in a greeting, it can be interpreted as a sign of distance or disdain.',
+      'incorrectResponse': '<b>No!</b> The correct answer is <b>D</b>. In Lebanon affection between men is common, and without sexual connotation. In fact, if a man chooses not to touch another in a greeting, it can be interpreted as a sign of distance or disdain.'
+    },
+    {
+      'q': '7. Greek male statues usually have small penises, because it is associated with …',
+      'options': [
+        'A.	Moderation and rationality',
+        'B.	Power and strength',
+        'C.	Beauty and elegance',
+		'D.	Compassion and wisdom'
+      ],
+      'correctIndex': 0,
+      'correctResponse': '<b>Right answer!</b> Ancient Greeks associated small and non-erect penises with sexual moderation, which was one of the key virtues that formed their view of ideal masculinity. It was believed in Ancient Greece, large penises were associated with lust and uncontrollable sexual urges.',
+      'incorrectResponse': '<b>Not really!</b> The correct answer is <b>A</b>. Ancient Greeks associated small and non-erect penises with sexual moderation, which was one of the key virtues that formed their view of ideal masculinity. It was believed in Ancient Greece, large penises were associated with lust and uncontrollable sexual urges.'
+    },
+	{
+      'q': '8. \"A sex symbol becomes a thing. I just hate being a thing.\" Which famous actresses  (widely considered to be a sex symbol) said this?',
+      'options': [
+        'A.	Elizabeth Taylor',
+        'B.	Audrey Hepburn',
+        'C.	Marilyn Monroe',
+		'D.	Brigitte Bardot'
+      ],
+      'correctIndex': 2,
+      'correctResponse': '<b>Correct!</b> \"I never quite understood it, this sex symbol, I always thought symbols were those things you clash together, \" remarked Marilyn Monroe in an interview in Ms. Magazine (August 1972).',
+      'incorrectResponse': '<b>Wrong!</b> The correct answer is <b>C</b>. \"I never quite understood it, this sex symbol, I always thought symbols were those things you clash together, \" remarked Marilyn Monroe in an interview in Ms. Magazine (August 1972).'
+    },
+	{
+      'q': '9. Which of the followings is an euphemism for male masturbation in Chinese (Cantonese and Mandarin) ?',
+      'options': [
+        'A.	Hit the aeroplane',
+        'B.	Hit the soya sauce',
+        'C.	Hit the fish',
+		'D.	Hit the telephone'
+      ],
+      'correctIndex': 0,
+      'correctResponse': '<b>Correct!</b> The phrase originally referred to the figurative motion of certain anti-aircraft missiles, which looks similar to masturbating.',
+      'incorrectResponse': '<b>No, it doesn’t mean that!</b> The correct answer is <b>A</b>. The phrase “hit the aeroplane” originally referred to the figurative motion of certain anti-aircraft missiles, which looks similar to masturbating.'
+    },
+	{
+      'q': '10. In the western tradition, Mars is the symbol of men as Venus is of women. Which planet has been adopted by transgender society as their symbol?',
+      'options': [
+        'A.	Jupiter',
+        'B.	Saturn',
+        'C.	Neptune',
+		'D.	Mercury'
+      ],
+      'correctIndex': 3,
+      'correctResponse': '<b>Right answer!</b> In Greek mythology, Aphrodite had a child with Mercury, who was named Hermaphroditus and possessed both male and female genitalia. Since Hermaphroditus didn\'t have a specific symbol, modern culture borrowed the symbol of Mercury to represent a transgender person.',
+      'incorrectResponse': '<b>Wrong answer!</b> The correct answer is <b>D</b>. In Greek mythology, Aphrodite had a child with Mercury, who was named Hermaphroditus and possessed both male and female genitalia. Since Hermaphroditus didn\'t have a specific symbol, modern culture borrowed the symbol of Mercury to represent a transgender person.'
+    }
+  ]
+});
